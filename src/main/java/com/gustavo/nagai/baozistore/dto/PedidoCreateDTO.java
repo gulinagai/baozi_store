1ab0d31b80447aa75008d1271e36ea19d38b6524
Gustavo Nagai Amorim <gulifiree@gmail.com>
2026-06-29 15:06:45 -0300

    ajuste validation no PedidoCreateDTO

--- src/main/java/com/gustavo/nagai/baozistore/dto/PedidoCreateDTO.java
+++ src/main/java/com/gustavo/nagai/baozistore/dto/PedidoCreateDTO.java
@@ -1,5 +1,7 @@
 package com.gustavo.nagai.baozistore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PedidoCreateDTO {
 
-
+    @NotNull(message = "O cliente é obrigatório.")
     private Long clienteId;
 
+    @NotNull(message = "O produto é obrigatório.")
     private Long produtoId;
 
+    @NotNull(message = "A quantidade é obrigatória.")
+    @Min(value = 1, message = "A quantidade deve ser maior que zero.")
     private Integer quantidade;
 
 }
